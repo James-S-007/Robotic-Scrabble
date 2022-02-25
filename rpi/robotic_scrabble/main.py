811bdc29d66f963faa15f8a117befeb40347b148
James Springer <jspringer8@gatech.edu>
2022-02-24 20:51:23 -0500

    State definitions pseudocode

---
+++ rpi/robotic_scrabble/main.py
@@ -0,0 +1,16 @@
+"""Main state machine and logic for robotic scrabble"""
+
+def main():
+    # State Definitions
+        # Setup State
+            # Distribute tiles
+            # Select start player randomly
+        # Gameplay States
+            # Player turn (waiting)
+            # Player turn complete (detect letters placed)
+            # Redistribute tiles
+            # AI Turn (read AI tiles, generate word to place and add to board)
+        # Reset State
+            # Put tiles back
+            # Display results
+    return
