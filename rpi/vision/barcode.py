06ac81cd9485efd45d8931c1d194d52e025afd2a
James-S-007 <jspringer8@gatech.edu>
2022-04-12 16:54:57 -0400

    Make vision module

--- rpi/vision/barcode.py
+++ rpi/vision/barcode.py
@@ -26,24 +26,28 @@ def draw_barcode(decoded, image):
                             thickness=5)
     return image
 
-cap = cv2.VideoCapture(1)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    frame = decode(frame)
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+def main():
+    cap = cv2.VideoCapture(1)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Our operations on the frame come here
+        frame = decode(frame)
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
+
+if __name__ == '__main__':
+    main()
