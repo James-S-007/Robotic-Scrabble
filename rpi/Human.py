877f4089a9dbc7ed1a6a4094bd018627be76ae76
James-S-007 <jspringer8@gatech.edu>
2022-04-13 12:54:51 -0400

    Modify to use camera object

--- rpi/Human.py
+++ rpi/Human.py
@@ -3,9 +3,9 @@
 '''
 
 import RPi.GPIO
+from time import sleep
 
 import halleffect
-from vision import vision
 
 class Human:
 
@@ -14,13 +14,16 @@ class Human:
         self.score = 0
 
     # records where human player places letters
-    def record_move(self, board, game_rules, end_turn_pb, cam):
+    def record_move(self, board, end_turn_pb, cam):
         move = {}
         hall_effect_arr = board.board.copy()
         prev_letter = curr_letter = None
         while not GPIO.input(end_turn_pb):
             prev_letter = curr_letter
-            curr_letter = vision.checkLetter(cam, ___)
+            curr_letter = None
+            while not curr_letter:
+                curr_letter = cam.check_letter()
+                sleep(.05)
             if move:   # not first turn, check where prev letter was placed
                 new_sensors = halleffect.check_new_sensors(hall_effect_arr)
                 if not new_sensors or len(new_sensors) > 1:
@@ -35,3 +38,8 @@ class Human:
         for sensor in new_sensors:
             hall_effect_arr[sensor[0]][sensor[1]] = 1  # update hall_effect_arr
         return move
+
+    # records move being made, scores word and updates board
+    def make_move(self, board, game_rules, end_turn_pb, cam):
+        move = self.record_move(board, end_turn_pb, cam)
+        
