eb78dd59374dbc2315a206e2bf8a3549d2e6f09a
James-S-007 <jspringer8@gatech.edu>
2022-04-18 13:30:13 -0400

    Init class commit

--- rpi/grbl/GrblStream.py
+++ rpi/grbl/GrblStream.py
@@ -30,33 +30,46 @@ THE SOFTWARE.
 """
 
 import serial
-import time
+from time import sleep
 import os.path
 
 # Open grbl serial port
 COM_PORT = 'COM5'
 s = serial.Serial(COM_PORT,115200)
 
-#print(s.readline()) # Debug (Ensure GRBL Start)
 
-# Wake up grbl
-s.write(str.encode("\r\n\r\n"))
-time.sleep(2)   # Wait for grbl to initialize 
-s.flushInput()  # Flush startup text in serial input
+class GrblStream:
+    def __init__(self, com_port):
+        self.s = serial.Serial(com_port, 115200)
+        s.write(str.encode("\r\n\r\n"))  # wake up grbl
+        self.gcode_path = os.path.join(os.path.dirname(__file__), 'grbl.gcode')
+        print('Waking up Grbl...')
+        sleep(2)
+        s.flushInput()
+        print('Grbl initalization complete')
+        
 
-# Open g-code file
-with open(os.path.join(os.path.dirname(__file__), 'grbl.gcode'),'r') as f:
-    # Stream g-code to grbl
-    for line in f:
-        l = line.strip() # Strip all EOL characters for consistency
-        print(f'Sending: {l}')
-        s.write(str.encode(l + '\n')) # Send g-code block to grbl
-        grbl_out = s.readline() # Wait for grbl response with carriage return
-        print (f'Response: {grbl_out.strip()}')
+    def __del__(self):
+        self.s.close()
 
+    # cmds = [(x, y), (x, y), ...]
+    def gen_gcode(self, cmds):
+        with open(self.gcode_path, 'w') as f:
+            f.write('$H\n')
+            for cmd in cmds:
+                f.write(f'G21 G90 X{cmd[0]} Y{cmd[1]} F5000\n')
+            
 
-# Wait here until grbl is finished to close serial port and file.
-#raw_input("  Press <Enter> to exit and disable grbl.") 
+    def send_gcode(self):
+        # Stream g-code to grbl
+        with open(self.gcode_path, 'r') as f:
+            for line in f:
+                l = line.strip() # Strip all EOL characters for consistency
+                print(f'Sending: {l}')
+                s.write(str.encode(l + '\n')) # Send g-code block to grbl
+                grbl_out = s.readline() # Wait for grbl response with carriage return
+                print (f'Response: {grbl_out.strip()}')
 
-# Close file and serial port
-s.close()
+    def gen_and_stream(self, cmds):
+        self.gen_gcode(cmds)
+        self.send_gcode()
