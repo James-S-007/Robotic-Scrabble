1a5b94b9de6f2d2edd6a407972cae2353e31dad0
James-S-007 <jspringer8@gatech.edu>
2022-04-24 23:53:30 -0400

    Finalish origin

--- rpi/config.py
+++ rpi/config.py
@@ -1,4 +1,4 @@
 COM_PORT = 'COM5'
-BOARD_ORIGIN = (300, 255)  # Gantry position for grid origin
+BOARD_ORIGIN = (147, 167)  # Gantry position for grid origin
 ORIGIN = (BOARD_ORIGIN[0] - 5*25.4, BOARD_ORIGIN[1] - 5*25.4)
 # ORIGIN = (103, 63)
