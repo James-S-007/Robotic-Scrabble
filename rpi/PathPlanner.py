c9f1e8aec734b4b94abbc005adc3b49fe4bc3d26
James-S-007 <jspringer8@gatech.edu>
2022-04-25 15:12:53 -0400

    Update a* bug

--- rpi/PathPlanner.py
+++ rpi/PathPlanner.py
@@ -7,11 +7,15 @@
 import os.path
 from pprint import pprint
 
+import heapq
+
 from scrabble.Board import Board
 
 
-class Node():
-    """A node class for A* Pathfinding"""
+class Node:
+    """
+    A node class for A* Pathfinding
+    """
 
     def __init__(self, parent=None, position=None):
         self.parent = parent
@@ -23,6 +27,17 @@ class Node():
 
     def __eq__(self, other):
         return self.position == other.position
+    
+    def __repr__(self):
+      return f"{self.position} - g: {self.g} h: {self.h} f: {self.f}"
+
+    # defining less than for purposes of heap queue
+    def __lt__(self, other):
+      return self.f < other.f
+    
+    # defining greater than for purposes of heap queue
+    def __gt__(self, other):
+      return self.f > other.f
 
 
 class PathPlanner():
@@ -84,14 +99,23 @@ class PathPlanner():
                     self.grid[i+offset[0]][j+offset[1]] = 1
 
 
-    def astar(self, start, end):
+    def return_path(self, current_node):
+        path = []
+        current = current_node
+        while current is not None:
+            path.append(current.position)
+            current = current.parent
+        return path[::-1]  # Return reversed path
+
+    def astar(self, start, end, allow_diagonal_movement=False):
         """Returns a list of tuples as a path from the given start to the given end in the given board"""
         # This is very jank but need a quick solution, will assume start/end are unoccupied for planning and reset after
         initial_start_state = self.grid[start[0]][start[1]]
         initial_end_state = self.grid[end[0]][end[1]]
         self.grid[start[0]][start[1]] = 0
         self.grid[end[0]][end[1]] = 0
-        # Create start and end node
+
+            # Create start and end node
         start_node = Node(None, start)
         start_node.g = start_node.h = start_node.f = 0
         end_node = Node(None, end)
@@ -101,39 +125,41 @@ class PathPlanner():
         open_list = []
         closed_list = []
 
-        # Add the start node
-        open_list.append(start_node)
+        # Heapify the open_list and Add the start node
+        heapq.heapify(open_list) 
+        heapq.heappush(open_list, start_node)
+
+        # Adding a stop condition
+        outer_iterations = 0
+        max_iterations = (len(self.grid[0]) * len(self.grid) // 2)
+
+        # what squares do we search
+        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
+        if allow_diagonal_movement:
+            adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1),)
 
         # Loop until you find the end
         while len(open_list) > 0:
+            outer_iterations += 1
 
+            if outer_iterations > max_iterations:
+                # if we hit this point return the path such as it is
+                # it will not contain the destination
+                print("giving up on pathfinding too many iterations")
+                return self.return_path(current_node)       
+            
             # Get the current node
-            current_node = open_list[0]
-            current_index = 0
-            for index, item in enumerate(open_list):
-                if item.f < current_node.f:
-                    current_node = item
-                    current_index = index
-
-            # Pop current off open list, add to closed list
-            open_list.pop(current_index)
+            current_node = heapq.heappop(open_list)
             closed_list.append(current_node)
 
             # Found the goal
             if current_node == end_node:
-                path = []
-                current = current_node
-                while current is not None:
-                    path.append(current.position)
-                    current = current.parent
-                # reset grid start and end points before returning
-                self.grid[start[0]][start[1]] = initial_start_state
-                self.grid[end[0]][end[1]] = initial_end_state
-                return path[::-1] # Return reversed path
+                return self.return_path(current_node)
 
             # Generate children
             children = []
-            for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+            
+            for new_position in adjacent_squares: # Adjacent squares
 
                 # Get node position
                 node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
@@ -154,11 +180,9 @@ class PathPlanner():
 
             # Loop through children
             for child in children:
-
                 # Child is on the closed list
-                for closed_child in closed_list:
-                    if child == closed_child:
-                        continue
+                if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                    continue
 
                 # Create the f, g, and h values
                 child.g = current_node.g + 1
@@ -166,12 +190,14 @@ class PathPlanner():
                 child.f = child.g + child.h
 
                 # Child is already in the open list
-                for open_node in open_list:
-                    if child == open_node and child.g > open_node.g:
-                        continue
+                if len([open_node for open_node in open_list if child.position == open_node.position and child.g > open_node.g]) > 0:
+                    continue
 
                 # Add the child to the open list
-                open_list.append(child)
+                heapq.heappush(open_list, child)
+
+        print("Couldn't get a path to destination")
+        return None
 
 
     # Reduces path from every point to manhattan
