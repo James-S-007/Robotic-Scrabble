5f69d46b644a614247c9c56a2322e5722ea2052b
James-S-007 <jspringer8@gatech.edu>
2022-04-25 23:19:27 -0400

    Dual platform support

--- rpi/config.py
+++ rpi/config.py
@@ -1,4 +1,11 @@
-COM_PORT = 'COM5'
-BOARD_ORIGIN = (152, 75)  # Gantry position for grid origin
+import platform
+
+os_info = platform.platform()
+COM_PORT = None
+if 'windows' in os_info.lower():
+    COM_PORT = 'COM5'
+elif 'linux' in os_info.lower():
+    COM_PORT = 'ACM0'
+BOARD_ORIGIN = (151, 85)  # Gantry position for grid origin
 ORIGIN = (BOARD_ORIGIN[0] - 4*25.4, BOARD_ORIGIN[1] - 4*25.4)
 # ORIGIN = (103, 63)
