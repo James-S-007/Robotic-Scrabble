f1ce505e72bc8e284f70646731eb1e35504a0b1b
James-S-007 <jspringer8@gatech.edu>
2022-04-25 23:54:35 -0400

    Fix Linux serial port

--- rpi/config.py
+++ rpi/config.py
@@ -5,7 +5,7 @@ COM_PORT = None
 if 'windows' in os_info.lower():
     COM_PORT = 'COM5'
 elif 'linux' in os_info.lower():
-    COM_PORT = 'ACM0'
-BOARD_ORIGIN = (151, 85)  # Gantry position for grid origin
+    COM_PORT = '/dev/ttyACM0'
+BOARD_ORIGIN = (136, 85)  # Gantry position for grid origin
 ORIGIN = (BOARD_ORIGIN[0] - 4*25.4, BOARD_ORIGIN[1] - 4*25.4)
 # ORIGIN = (103, 63)
